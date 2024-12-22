b19b2610fe2a05ee333804fe08040f95eceb1dbf
[Marcel Prats Ustrell] <marcelprats@uoc.edu>
2024-12-22 20:15:14 +0100

    Formulari reactiu per afegir articles amb validacions, segueix l'error anterior

--- PEC5_Ej_Prac/ecommerce/src/app/app.module.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
-import { NavbarModule } from './navbar/navbar.module';
-import { ArticleListComponent } from './article-list/article-list.component';
-import { ArticleNewTemplateComponent } from './article-new-template/article-new-template.component';
-import { ArticleNewReactiveComponent } from './article-new-reactive/article-new-reactive.component';
-import { ArticleTemplateFormComponent } from './article-template-form/article-template-form.component';
-import { FormsModule } from '@angular/forms';
-
+import { NavbarModule } from './navbar/navbar.module'; 
+import { ArticleListComponent } from './article-list/article-list.component'; 
+import { ArticleTemplateFormComponent } from './article-template-form/article-template-form.component'; 
+import { ArticleNewReactiveComponent } from './article-new-reactive/article-new-reactive.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    ArticleListComponent, 
-    ArticleNewTemplateComponent, 
-    ArticleNewReactiveComponent,
-    ArticleTemplateFormComponent
+    AppComponent,
+    ArticleListComponent,
+    ArticleTemplateFormComponent,
+    ArticleNewReactiveComponent
   ],
   imports: [
     BrowserModule,
-    NavbarModule,
-    FormsModule
+    FormsModule, 
+    ReactiveFormsModule, 
+    NavbarModule 
   ],
   providers: [],
-  bootstrap: [AppComponent] 
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- PEC5_Ej_Prac/ecommerce/src/app/article-new-reactive/article-new-reactive.component.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/article-new-reactive/article-new-reactive.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-article-new-reactive',
-  imports: [],
   templateUrl: './article-new-reactive.component.html',
-  styleUrl: './article-new-reactive.component.css'
+  styleUrls: ['./article-new-reactive.component.css']
 })
 export class ArticleNewReactiveComponent {
+  articleForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.articleForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      price: [null, [Validators.required, Validators.min(0.1)]],
+      imageUrl: ['', [Validators.required, Validators.pattern(/https?:\/\/[\w\-\.]+\.\w{2,}(\/.*)?/)]],
+      isOnSale: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.articleForm.valid) {
+      console.log('Formulari reactiu enviat:', this.articleForm.value);
+    } else {
+      console.log('El formulari és invàlid');
+    }
+  }
 }
